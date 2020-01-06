860b7d9fa8be9367f1741fbdc930f8fb8a4591bb
ChebnevDev <chebnevdev@gmail.com>
2020-01-06 11:36:45 +0300

    Added MqWrapper library and testing project

---
+++ AirportProject/MqWrapper/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MqWrapper
+{
+    public class Class1
+    {
+    }
+}
---
+++ AirportProject/MqWrapperTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MqWrapperTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
