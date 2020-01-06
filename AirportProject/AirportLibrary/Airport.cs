ad385bb8c1c87b69d2c85e00653147a55ac53ef4
ChebnevDev <chebnevdev@gmail.com>
2020-01-06 17:11:20 +0300

    Clean

--- AirportProject/AirportLibrary/Airport.cs
+++ AirportProject/AirportLibrary/Airport.cs
@@ -4,17 +4,6 @@ namespace AirportLibrary
 {
     public static class Airport
     {
-        public static class Something
-        {
-            public static class More
-            {
-                public const string Const = "Hi";
-            }
-        }
-
-        public static void Main(String[] args)
-        {
-            Console.WriteLine(Airport.Something.More.Const);
-        }
+        
     }
 }
