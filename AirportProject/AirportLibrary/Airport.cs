4fdaf1d6871057dc9a7e5d42ba7e62c9a9b88ca3
ChebnevDev <chebnevdev@gmail.com>
2020-01-06 22:59:13 +0300

    Added almost perfect enum solution

--- AirportProject/AirportLibrary/Airport.cs
+++ AirportProject/AirportLibrary/Airport.cs
@@ -2,8 +2,52 @@
 
 namespace AirportLibrary
 {
+    public enum Component
+    {
+        Schedule,
+        Airplane,
+        GroundService,
+        Timetable,
+        Cashbox,
+        Registration,
+        Storage,
+        Passenger,
+        GroundMotion,
+        Bus,
+        Baggage,
+        FollowMe,
+        Catering,
+        Deicing,
+        FuelTruck,
+        TimeService,
+        Visualizer,
+        Logs,
+    }
     public static class Airport
     {
         
+    }
+
+    // Class for generating queue names
+    public class Queue
+    {
+        Component from;
+        private Queue(Component from)
+        {
+            this.from = from;
+        }
+        public static Queue From(Component from)
+        {
+            return new Queue(from);
+        }
+        public string To(Component to)
+        {
+            return from.ToString() + "-" + to.ToString();
+        }
+        public static string FromAnyTo(Component to)
+        {
+            return to.ToString();
+        }
+
     }
 }
--- AirportProject/ScheduleComponent/Program.cs
+++ AirportProject/ScheduleComponent/Program.cs
@@ -4,6 +4,7 @@ using RabbitMQ.Client.Events;
 using System;
 using System.Text;
 using System.Threading;
+using AirportLibrary;
 
 namespace ScheduleComponent
 {
@@ -12,38 +13,13 @@ namespace ScheduleComponent
         const string QUEUE_NAME = "schedule-cashbox";
         static void Main(string[] args)
         {
-            var mqClient = new RabbitMqClient();
-
-            mqClient.DeclareQueues(QUEUE_NAME);
-
-            var message = new MyCustomMessage()
-            {
-                MyTime = DateTime.Now,
-                MyString = "Hello World, This is a test",
-                MyArray = new int[] { 1, 2, 3, 44, 42 }
-            };
-
-            mqClient.Send(QUEUE_NAME, message);
-
-            mqClient.SubscribeTo<MyCustomMessage>(QUEUE_NAME, (mes) =>
-            {
-                Console.WriteLine("{0} Received: {1}", DateTime.Now, mes);
-            });
-
-            Console.ReadLine();
-            mqClient.Dispose();
+            Console.WriteLine(Queue.From(Component.Airplane).To(Component.Baggage));
+            Console.WriteLine(Queue.FromAnyTo(Component.Logs));
         }
     }
 
     class MyCustomMessage
     {
-        public DateTime MyTime { get; set; }
-        public string MyString { get; set; }
-        public int[] MyArray { get; set; }
-
-        public override string ToString()
-        {
-            return MyTime.ToString() + "; " + MyString + "; " + String.Join(", ", MyArray);
-        }
+        
     }
 }
