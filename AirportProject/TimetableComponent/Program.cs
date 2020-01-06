cc5eac2f61f844ffd55a75005fb9952fe8fc31e5
ChebnevDev <chebnevdev@gmail.com>
2020-01-06 11:29:55 +0300

    Merge

--- AirportProject/TimetableComponent/Program.cs
+++ AirportProject/TimetableComponent/Program.cs
@@ -1,18 +1,15 @@
-﻿using RabbitMqWrapper;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
-using System.Threading;
-using AirportLibrary;
 
-namespace ScheduleComponent
+namespace TimetableComponent
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            new TimetableComponent().Start();
         }
     }
 }
