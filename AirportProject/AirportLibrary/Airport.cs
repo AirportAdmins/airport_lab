97bad5593c519fbe11a192ada3814662e7ae32ff
ChebnevDev <chebnevdev@gmail.com>
2020-01-06 17:08:54 +0300

    Added AirportLibrary

---
+++ AirportProject/AirportLibrary/Airport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AirportLibrary
+{
+    public static class Airport
+    {
+        public static class Something
+        {
+            public static class More
+            {
+                public const string Const = "Hi";
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(Airport.Something.More.Const);
+        }
+    }
+}
---
+++ AirportProject/AirportLibrary/ConfigManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace AirportLibrary.Config
+{
+    public static class ConfigManager
+    {
+        public static string HostName
+        {
+            get => ConfigurationManager.AppSettings["hostname"];
+        }
+        public static string UserName
+        {
+            get => ConfigurationManager.AppSettings["username"];
+        }
+        public static string Password {
+            get => ConfigurationManager.AppSettings["password"];
+        }
+    }
+}
