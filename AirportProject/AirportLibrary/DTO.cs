8fb0c24ab4235ea3a531d98cf99a5bd1b5bbc7ef
ChebnevDev <chebnevdev@gmail.com>
2020-01-07 16:31:09 +0300

    Added DTO for GroundMotion and GroundService

--- AirportProject/AirportLibrary/DTO.cs
+++ AirportProject/AirportLibrary/DTO.cs
@@ -76,7 +76,7 @@ namespace AirportLibrary.DTO
     // Airplane Component
     // ===================================
     // With GroundService
-    public class AirplaneServiceRequest
+    public class AirplaneServiceCommand
     {
         public string PlaneId { get; set; }
         public int LocationVertex { get; set; }
@@ -228,6 +228,86 @@ namespace AirportLibrary.DTO
     }
     // ===================================
 
+    // GroundService Component
+    // ===================================
+    // With Storage
+    public class FlightStorageInfoRequest
+    {
+        public string FlightId { get; set; }
+    }
+    public class FlightStorageInfoResponse
+    {
+        public string FlightId { get; set; }
+        public int PassengersCount { get; set; }
+        public int BaggageCount { get; set; }
+    }
+    // Common to Service Commands
+    public class ServiceCommand
+    {
+        public int PlaneLocationVertex { get; set; }
+        public string PlaneId { get; set; }
+    }
+    // With FollowMe
+    public class AirplaneTransferCommand : ServiceCommand
+    {
+        public int DestinationVertex { get; set; }
+    }
+    // With Bus
+    public class PassengersServiceCommand : ServiceCommand
+    {
+        public int StorageVertex { get; set; }
+        public TransferAction Action { get; set; }
+        public int PassengersCount { get; set; }
+        public string FlightId { get; set; }
+    }
+    // With Baggage
+    public class BaggageServiceCommand : ServiceCommand
+    {
+        public int StorageVertex { get; set; }
+        public TransferAction Action { get; set; }
+        public int BaggageCount { get; set; }
+        public string FlightId { get; set; }
+    }
+    // With FuelTruck
+    public class RefuelServiceCommand : ServiceCommand
+    {
+        public int Fuel { get; set; }
+    }
+    // With Catering
+    public class CateringServiceCommand : ServiceCommand
+    {
+        public List<Tuple<Food, int>> FoodList { get; set; }
+    }
+    // With all service cars
+    public class ServiceCompletionMessage
+    {
+        // Constant from AirportLibrary.Component class
+        public string Component { get; set; }
+        public string PlaneId { get; set; }
+    }
+    // ===================================
+
+    // GroundMotion Component
+    // ===================================
+    // With all transport
+    public class MotionPermissionRequest
+    {
+        // Constant from AirportLibrary.Component class
+        public string Component { get; set; }
+        public string ObjectId { get; set; }
+        public int StartVertex { get; set; }
+        public int DestinationVertex { get; set; }
+        public MotionAction Action { get; set; }
+    }
+    public enum MotionAction
+    {
+        Occupy, Free
+    }
+    public enum MotionPermissionResponse
+    {
+        Positive
+    }
+    // ===================================
 
     // TimeService Component
     // ===================================
