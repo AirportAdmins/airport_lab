ab0fd26c6710426bec2ddd3d36d8d3a2b9d78834
ostryaninam <anna.ostryanina@gmail.com>
2020-01-07 23:22:11 +0300

    baggage response

--- AirportProject/AirplaneComponent/PCAirplane.cs
+++ AirportProject/AirplaneComponent/PCAirplane.cs
@@ -4,6 +4,7 @@ using System.Text;
 using RabbitMqWrapper;
 using AirplaneComponent.AirplaneGenerator;
 using AirportLibrary.DTO;
+using AirportLibrary;
 
 namespace AirplaneComponent
 {
@@ -11,18 +12,34 @@ namespace AirplaneComponent
     {
         RabbitMqClient MqClient;
         Dictionary<string, Airplane> airplanes;
-        string[] queues = new string[] { "Aiplane-Schedule","Airplane-Bus" };
+        Dictionary<string, string> queues;
         public PCAirplane()
         {
             MqClient = new RabbitMqClient();
         }
 
+        void FillQueues()
+        {
+            queues = new Dictionary<string, string>();
+            queues.Add(Component.Schedule, Component.Airplane + "-" + Component.Schedule);
+            queues.Add(Component.Bus, Component.Airplane + "-" + Component.Bus);
+            queues.Add(Component.Baggage, Component.Airplane + "-" + Component.Baggage);
+        }
+        
+        void DeclareQueues()
+        {
+            string[] queuesString = new string[3];
+            queues.Values.CopyTo(queuesString,0);
+            MqClient.DeclareQueues(queuesString);
+        }
         void SubscribeAll()
         {
-            MqClient.SubscribeTo<AirplaneGenerationRequest>(queues[0], mes =>   //schedule
+            MqClient.SubscribeTo<AirplaneGenerationRequest>(queues[Component.Schedule], mes =>   //schedule
                      ScheduleResponse(mes));
-            MqClient.SubscribeTo<PassengerTransferRequest>(queues[1], mes =>    //bus
+            MqClient.SubscribeTo<PassengerTransferRequest>(queues[Component.Bus], mes =>    //bus
                      BusTransferResponse(mes));
+            MqClient.SubscribeTo<BaggageTransferRequest>(queues[Component.Baggage], mes =>    //baggage
+                     BaggageTransferResponse(mes));
         }
         void ScheduleResponse(AirplaneGenerationRequest req)
         {
@@ -36,18 +53,36 @@ namespace AirplaneComponent
         }
         void BusTransferResponse(PassengerTransferRequest req)
         {
+            Airplane plane = airplanes[req.PlaneId];
             if (req.Action == TransferAction.Take)
             {
-                MqClient.Send<PassengersTransfer>(queues[1], new PassengersTransfer()
+                MqClient.Send<PassengersTransfer>(queues[Component.Bus], new PassengersTransfer()
                 {
                     BusId = req.BusId,
-                    PassengerCount = airplanes[req.PlaneId].Passengers
+                    PassengerCount = req.PassengersCount
                 });
-                airplanes[req.PlaneId].Passengers = 0;
+                plane.Passengers -= req.PassengersCount;
             }
             else
             {
-                airplanes[req.PlaneId].Passengers = req.PassengersCount;
+                plane.Passengers += req.PassengersCount;
+            }
+        }
+        void BaggageTransferResponse(BaggageTransferRequest req)
+        {
+            Airplane plane = airplanes[req.PlaneId];
+            if (req.Action == TransferAction.Take)
+            {
+                MqClient.Send<BaggageTransfer>(queues[Component.Baggage], new BaggageTransfer()
+                {
+                    BaggageCarId = req.BaggageCarId,
+                    BaggageCount = req.BaggageCount
+                });
+                plane.BaggageAmount -= req.BaggageCount;
+            }
+            else
+            {
+                plane.BaggageAmount += req.BaggageCount;
             }
         }
     } 
