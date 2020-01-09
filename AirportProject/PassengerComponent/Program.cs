35fec0071a4a8eaaa5b1be014210a2f93f972c15
ChebnevDev <chebnevdev@gmail.com>
2020-01-10 02:45:39 +0300

    Added passenger component

---
+++ AirportProject/PassengerComponent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PassengerComponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new PassengerComponent().Start();
+        }
+    }
+}
