9f74a6e0de5e9e1629f1706fbccf3a0051aa9a6c
Fedor Chebnev <chebnevdev@gmail.com>
2020-01-10 23:46:00 +0300

    Added queues

--- AirportProject/PassengerComponent/PassengerComponent.cs
+++ AirportProject/PassengerComponent/PassengerComponent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +6,35 @@ namespace PassengerComponent
 {
     class PassengerComponent
     {
+        public const PassengerToCashboxQueue = Component.Passenger + Component.Cashbox;
+        public const PassengerToRegistrationQueue = Component.Passenger + Component.Registration;
+        public const PassengerToLogsQueue = Component.Passenger + Component.Logs; 
+
+        public const TimetableToPassengerQueue = Component.Timetable + Component.Passenger;
+        public const CashboxToPassengerQueue = Component.Cashbox + Component.Passenger;
+        public const RegistrationToPassengerQueue = Component.Registration + Component.Passenger;
+        public const StorageToPassengerQueue = Component.Storage + Component.Passenger;
+        public const BusToPassengerQueue = Component.Bus + Component.Passenger;
+        public const TimeServiceToPassengerQueue = Component.TimeService + Component.Passenger;
+
+        public static readonly List<string> queues = new List<string>
+        {
+            PassengerToCashboxQueue,
+            PassengerToRegistrationQueue,
+            PassengerToLogsQueue,
+            TimetableToPassengerQueue,
+            CashboxToPassengerQueue,
+            RegistrationToPassengerQueue,
+            StorageToPassengerQueue,
+            BusToPassengerQueue,
+            TimeServiceToPassengerQueue
+        };
+
         public void Start()
         {
+            var mqClient = new RabbitMqClient();
+
+            mqClient.
 
         }
     }
