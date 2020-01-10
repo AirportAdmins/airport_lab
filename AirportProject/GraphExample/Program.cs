5638b1a7b72812ddee41a1afcd512b6f6ca793cd
shelmets <anikolaev98@icloud.com>
2020-01-10 21:36:48 +0300

    Added class Map in AirportLibrary/Airport.cs

---
+++ AirportProject/GraphExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
