8e9893a6f84c43eca22320f9489248853753ddd2
ChebnevDev <chebnevdev@gmail.com>
2020-01-10 03:24:12 +0300

    Implemented Timetable

---
+++ AirportProject/TimetableComponent/Timetable.cs
@@ -0,0 +1,55 @@
+﻿using AirportLibrary.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimetableComponent
+{
+    interface ITimetable
+    {
+        void SetCurrentTime(DateTime currentTime);
+        void UpdateFlight(FlightStatusUpdate flight);
+        void Draw();
+    }
+    class ConsoleTimetable : ITimetable
+    {
+        List<FlightStatusUpdate> flights;
+        DateTime currentTime;
+
+        public void Draw()
+        {
+            Console.Clear();
+            Console.WriteLine(currentTime);
+            Console.WriteLine("======================================");
+            Console.WriteLine("||Flight || Status || Departure time||");
+            Console.WriteLine("======================================");
+            foreach (var flight in flights)
+            {
+                Console.WriteLine("||{0} {1} {2}||", 
+                    flight.FlightId, 
+                    flight.Status,  
+                    flight.Status == FlightStatus.Delayed ? "" : flight.DepartureTime.ToString()
+                );
+            }
+            Console.WriteLine("======================================");
+        }
+
+        public void SetCurrentTime(DateTime currentTime)
+        {
+            this.currentTime = currentTime;
+        }
+
+        public void UpdateFlight(FlightStatusUpdate flight)
+        {
+            for (int i = 0; i < flights.Count; i++)
+            {
+                if (flights[i].FlightId == flight.FlightId)
+                {
+                    flights[i].Status = flight.Status;
+                    return;
+                }
+            }
+            flights.Add(flight);
+        }
+    }
+}
--- AirportProject/TimetableComponent/TimetableComponent.cs
+++ AirportProject/TimetableComponent/TimetableComponent.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AirportLibrary;
+using AirportLibrary.DTO;
+using RabbitMqWrapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +9,43 @@ namespace TimetableComponent
 {
     class TimetableComponent
     {
+        public const string TimetableToPassengerQueue =
+            Component.Timetable + Component.Passenger;
+
+        public const string ScheduleToTimetableQueue =
+            Component.Timetable + Component.Passenger;
+        public const string TimeServiceToTimetableQueue =
+            Component.TimeService + Component.Passenger;
+
         public void Start()
         {
+            var mqClient = new RabbitMqClient();
 
+            mqClient.DeclareQueues(
+                TimetableToPassengerQueue,
+                ScheduleToTimetableQueue,
+                TimeServiceToTimetableQueue
+            );
+
+            mqClient.PurgeQueues(
+                TimetableToPassengerQueue,
+                ScheduleToTimetableQueue,
+                TimeServiceToTimetableQueue
+            );
+
+            var timetable = new ConsoleTimetable();
+
+            mqClient.SubscribeTo<CurrentPlayTime>(TimeServiceToTimetableQueue, (mes) =>
+            {
+                timetable.SetCurrentTime(mes.PlayTime);
+                timetable.Draw();
+            });
+
+            mqClient.SubscribeTo<FlightStatusUpdate>(ScheduleToTimetableQueue, (mes) =>
+            {
+                timetable.UpdateFlight(mes);
+
+            });
         }
     }
 }
