9a092e69fe8522c326a9aea6e0cd54c166ec6eac
ostryaninam <anna.ostryanina@gmail.com>
2020-01-11 12:48:12 +0300

    some subscribe

--- AirportProject/FollowMeComponent/FollowMeComponent.cs
+++ AirportProject/FollowMeComponent/FollowMeComponent.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirportLibrary;
+using RabbitMqWrapper;
 
 namespace FollowMeComponent
 {
-    class FollowMeComponent
+    public class FollowMeComponent
     {
+        Dictionary<string, string> queuesFrom;
+        Dictionary<string, string> queuesTo;
+        RabbitMqClient MqClient;
+        public FollowMeComponent()
+        {
+            MqClient = new RabbitMqClient();
+        }
+        public void Start()
+        {
+            CreateQueues();
+            DeclareQueues();
+        }
+        void CreateQueues()
+        {
+            queuesFrom = new Dictionary<string, string>()
+            {
+                { Component.GroundMotion,Component.GroundMotion+Component.FollowMe },
+                { Component.Airplane,Component.Airplane+Component.FollowMe },
+                { Component.GroundService,Component.GroundService+Component.FollowMe },
+                { Component.TimeService,Component.TimeService + Component.FollowMe }
+            };
+            queuesTo = new Dictionary<string, string>()
+            {
+                { Component.Airplane,Component.FollowMe+Component.Airplane },
+                { Component.Logs,Component.Logs+Component.FollowMe },
+                { Component.GroundService,Component.FollowMe+Component.GroundService },
+                { Component.GroundMotion,Component.FollowMe+Component.GroundMotion },
+                { Component.Visualizer,Component.FollowMe+Component.Visualizer }
+            };
+        }
+        void DeclareQueues()
+        {
+            MqClient.DeclareQueues(queuesFrom.Values.ToArray());
+            MqClient.DeclareQueues(queuesTo.Values.ToArray());
+        }
     }
 }
