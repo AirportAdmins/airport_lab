1547c0391ca0033d4d7febe4f876651216d6f431
Fedor Chebnev <chebnevdev@gmail.com>
2020-01-11 13:00:55 +0300

    Added queue initialization

--- AirportProject/PassengerComponent/PassengerComponent.cs
+++ AirportProject/PassengerComponent/PassengerComponent.cs
@@ -1,21 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AirportLibrary;
+using RabbitMqWrapper;
 
 namespace PassengerComponent
 {
     class PassengerComponent
     {
-        public const PassengerToCashboxQueue = Component.Passenger + Component.Cashbox;
-        public const PassengerToRegistrationQueue = Component.Passenger + Component.Registration;
-        public const PassengerToLogsQueue = Component.Passenger + Component.Logs; 
-
-        public const TimetableToPassengerQueue = Component.Timetable + Component.Passenger;
-        public const CashboxToPassengerQueue = Component.Cashbox + Component.Passenger;
-        public const RegistrationToPassengerQueue = Component.Registration + Component.Passenger;
-        public const StorageToPassengerQueue = Component.Storage + Component.Passenger;
-        public const BusToPassengerQueue = Component.Bus + Component.Passenger;
-        public const TimeServiceToPassengerQueue = Component.TimeService + Component.Passenger;
+        public const string PassengerToCashboxQueue = Component.Passenger + Component.Cashbox;
+        public const string PassengerToRegistrationQueue = Component.Passenger + Component.Registration;
+        public const string PassengerToLogsQueue = Component.Passenger + Component.Logs; 
+                      
+        public const string TimetableToPassengerQueue = Component.Timetable + Component.Passenger;
+        public const string CashboxToPassengerQueue = Component.Cashbox + Component.Passenger;
+        public const string RegistrationToPassengerQueue = Component.Registration + Component.Passenger;
+        public const string StorageToPassengerQueue = Component.Storage + Component.Passenger;
+        public const string BusToPassengerQueue = Component.Bus + Component.Passenger;
+        public const string TimeServiceToPassengerQueue = Component.TimeService + Component.Passenger;
 
         public static readonly List<string> queues = new List<string>
         {
@@ -34,8 +38,32 @@ namespace PassengerComponent
         {
             var mqClient = new RabbitMqClient();
 
-            mqClient.
+            mqClient.DeclareQueues(
+                queues.ToArray()
+            );
 
+            mqClient.PurgeQueues(
+                queues.ToArray()
+            );
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            Task.Run(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    // generate passengers
+                }
+            }, cancellationToken);
+
+            Task.Run(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    // send passengers to do something
+                }
+            }, cancellationToken);
         }
     }
 }
