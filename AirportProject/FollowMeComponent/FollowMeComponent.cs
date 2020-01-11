3c5c406d6def162ce59b4818d47a62d6d75fcf7e
ostryaninam <anna.ostryanina@gmail.com>
2020-01-11 12:52:03 +0300

    Added MotionPermissionResponse to DTO

---
+++ AirportProject/FollowMeComponent/FollowMeComponent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirportLibrary.DTO;
+using AirportLibrary;
+using RabbitMqWrapper;
+
+namespace FollowMeComponent
+{
+    public class FollowMeComponent
+    {
+        Dictionary<string, string> queuesFrom;
+        Dictionary<string, string> queuesTo;
+        RabbitMqClient MqClient;
+        double TimeSpeedFactor = 1;
+        public FollowMeComponent()
+        {
+            MqClient = new RabbitMqClient();
+        }
+        public void Start()
+        {
+            CreateQueues();
+            DeclareQueues();
+        }
+        void CreateQueues()
+        {
+            queuesFrom = new Dictionary<string, string>()
+            {
+                { Component.GroundMotion,Component.GroundMotion+Component.FollowMe },
+                { Component.Airplane,Component.Airplane+Component.FollowMe },
+                { Component.GroundService,Component.GroundService+Component.FollowMe },
+                { Component.TimeService,Component.TimeService + Component.FollowMe }
+            };
+            queuesTo = new Dictionary<string, string>()
+            {
+                { Component.Airplane,Component.FollowMe+Component.Airplane },
+                { Component.Logs,Component.Logs+Component.FollowMe },
+                { Component.GroundService,Component.FollowMe+Component.GroundService },
+                { Component.GroundMotion,Component.FollowMe+Component.GroundMotion },
+                { Component.Visualizer,Component.FollowMe+Component.Visualizer }
+            };
+        }
+        void Subscribe()
+        {
+            MqClient.SubscribeTo<AirplaneTransferCommand>(queuesFrom[Component.GroundService], cmd =>
+                    AirplaneTransfer(cmd));
+            MqClient.SubscribeTo<NewTimeSpeedFactor>(queuesFrom[Component.TimeService], mes =>
+                    TimeSpeedFactor = mes.Factor);
+        }
+        void DeclareQueues()
+        {
+            MqClient.DeclareQueues(queuesFrom.Values.ToArray());
+            MqClient.DeclareQueues(queuesTo.Values.ToArray());
+        }
+        void AirplaneTransfer(AirplaneTransferCommand cmd)
+        {
+
+        }
+    }
+}
