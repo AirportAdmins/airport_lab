0310690a1dd7e281342944d630afcf0d77f6f1b3
shelmets <anikolaev98@icloud.com>
2020-01-12 19:15:55 +0300

    Added queues in Groundmoution

---
+++ AirportProject/GroundmoutionComponent/GroundmoutionComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using AirportLibrary.DTO;
+using AirportLibrary;
+using RabbitMqWrapper;
+
+namespace GroundmoutionComponent
+{
+    public class GroundmoutionComponent
+    {
+        public static readonly string SendersToGroundmoutionQueue = Component.GroundMotion;
+
+        public static readonly List<string> MotionPermissionSenders = new List<string>()
+        {
+            Component.Airplane,
+            Component.Bus,
+            Component.Catering,
+            Component.Deicing,
+            Component.FollowMe,
+            Component.FuelTruck
+        };
+
+        public static readonly List<string> MotionPermissionReceivers = MotionPermissionSenders;
+
+        RabbitMqClient mqClient = new RabbitMqClient();
+
+        GroundmoutionQueues groundmoution = new GroundmoutionQueues();
+
+        public GroundmoutionComponent()
+        {
+        }
+        public void Start()
+        {
+            //declare queue SendersToGroundmoution
+            mqClient.DeclareQueues(SendersToGroundmoutionQueue);
+
+            //declare queues GroundmoutionToReceivers
+            foreach (var receiver in MotionPermissionReceivers)
+            {
+                mqClient.DeclareQueues(receiver + Component.GroundMotion);
+            }
+
+            mqClient.SubscribeTo<MotionPermissionRequest>(SendersToGroundmoutionQueue, )
+
+        }
+    }
+}
---
+++ AirportProject/GroundmoutionComponent/GroundmoutionQueues.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using AirportLibrary.Graph;
+using AirportLibrary.DTO;
+using AirportLibrary;
+
+namespace GroundmoutionComponent
+{
+    class GroundmoutionQueues
+    {
+        public Dictionary<Edge<int>, Queue<MotionPermissionRequest>> dictBusyEdges { get; }
+
+        public GroundmoutionQueues()
+        {
+            dictBusyEdges = new Dictionary<Edge<int>, Queue<MotionPermissionRequest>>();
+            foreach (var edge in new Map().Graph.Edges)
+                dictBusyEdges.Add(edge, new Queue<MotionPermissionRequest>());
+        }
+
+        private Edge<int> FindEdge(int vertexFrom, int vertexTo)
+        {
+            foreach (var edge in dictBusyEdges.Keys)
+                if (edge.ConnVertices.Item1.Id == vertexFrom && edge.ConnVertices.Item2.Id == vertexTo || edge.ConnVertices.Item2.Id == vertexFrom && edge.ConnVertices.Item1.Id == vertexTo)
+                    return edge;
+            return null;
+        }
+
+        public bool Enqueue(MotionPermissionRequest request)
+        {
+            var edge = FindEdge(request.StartVertex, request.DestinationVertex);
+            if (edge != null)
+            {
+                dictBusyEdges[edge].Enqueue(request);
+            }
+            return edge!=null;
+        }
+
+        public MotionPermissionRequest Dequeue(int vertexFrom, int vertexTo)
+        {
+            var edge = FindEdge(vertexFrom, vertexTo);
+            if (edge != null)
+            {
+                return dictBusyEdges[edge].Dequeue();
+            }
+            return null;
+        }
+
+    }
+}
