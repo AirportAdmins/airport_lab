0a16a43345cc488924520c8598f2347d32b36af7
shelmets <anikolaev98@icloud.com>
2020-01-13 02:06:19 +0300

    Added GroundServiceComp

---
+++ AirportProject/GroundServiceComponent/GroundServiceComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GroundServiceComponent
+{
+    public class GroundServiceComponent
+    {
+
+        public GroundServiceComponent()
+        {
+        }
+    }
+}
---
+++ AirportProject/GroundServiceComponent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroundServiceComponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
