167d11584e26b1efbc98176617fb3da92252a3e1
shelmets <anikolaev98@icloud.com>
2020-01-13 23:24:23 +0300

    Declare queues

--- AirportProject/GroundServiceComponent/GroundServiceComponent.cs
+++ AirportProject/GroundServiceComponent/GroundServiceComponent.cs
@@ -1,11 +1,55 @@
 ﻿using System;
+using AirportLibrary;
+using System.Collections.Generic;
+using RabbitMqWrapper;
+
 namespace GroundServiceComponent
 {
     public class GroundServiceComponent
     {
+        public static readonly string ComponentName = Component.GroundService;
+        public static readonly List<string> Receivers = new List<string>()
+        {
+            Component.Airplane,
+            Component.FollowMe,
+            Component.Bus,
+            Component.Baggage,
+            Component.FuelTruck,
+            Component.Catering,
+            Component.Deicing,
+            Component.Timetable
+        };
+        public static readonly List<string> Senders = new List<string>()
+        {
+            Component.Airplane,
+            Component.FollowMe,
+            Component.Bus,
+            Component.Baggage,
+            Component.FuelTruck,
+            Component.Catering,
+            Component.Deicing,
+            Component.Timetable,
+            Component.Registration
+        };
 
+        RabbitMqClient mqClient = new RabbitMqClient();
+        
         public GroundServiceComponent()
         {
+        }
+        public void FisrtCycle()
+        {
+
+        }
+        public void SecondCycle()
+        {
+
+        }
+        public void Start()
+        {
+            foreach (var sender in Senders)
+                mqClient.DeclareQueues(sender+ComponentName);
+
         }
     }
 }
