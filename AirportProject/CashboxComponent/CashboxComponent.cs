b1755b5a1c9d66e135002cb656cb46289582115f
ChebnevDev <chebnevdev@gmail.com>
2020-01-19 23:35:50 +0300

    Minor changes

---
+++ AirportProject/CashboxComponent/CashboxComponent.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMqWrapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashboxComponent
+{
+    class CashboxComponent
+    {
+        RabbitMqClient mqClient = new RabbitMqClient();
+
+        public void Start()
+        {
+            mqClient.DeclareQueues(queues.ToArray());
+            mqClient.PurgeQueues(queues.ToArray());
+
+
+        }
+    }
+}
