47c97386fd7f13b55afcd01c5cda2b1c2d650a78
NataleeNovak <natalianovak84@gmail.com>
2020-01-22 23:27:05 +0300

    Added files for Timetable

---
+++ AirportProject/TimetableComponent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace TimetableComponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TimetableComponent().Start();
+        }
+    }
+}
