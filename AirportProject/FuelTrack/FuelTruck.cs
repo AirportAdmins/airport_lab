e59cb5e58f4363760ddd0cec114f81f66404f5ee
Shanarov <bilibord@mail.ru>
2020-01-22 23:11:40 +0300

    temporary changes

--- AirportProject/FuelTrack/FuelTruck.cs
+++ AirportProject/FuelTrack/FuelTruck.cs
@@ -1,232 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Collections.Concurrent;
+using System.Text;
+using System.Linq;
 using RabbitMqWrapper;
 using AirportLibrary;
 using AirportLibrary.DTO;
 using AirportLibrary.Delay;
 using AirportLibrary.Graph;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace FuelTruck
 {
+    delegate void GoToVertexAction(FuelTruckCar fueltruck, int DestinationVertex);
     public class FuelTruck
     {
-        //Получает сообщения
-        const string queueFromTimeService = Component.TimeService + Component.FuelTruck; //Так ли получать сообщения? или как у СНД?
-        const string queueFromGroundService = Component.GroundService + Component.FuelTruck; //рэди
-        const string queueFromGroundMotion = Component.GroundMotion + Component.FuelTruck;
-
-        //Отправляет сообщения
-        const string queueToAirPlane = Component.FuelTruck + Component.Airplane;
-        const string queueToLogs = Component.FuelTruck + Component.Logs; //так отправлять логи? или как у СНД?
-        const string queueToGroundMotion = Component.GroundMotion;
-        const string queueToGroundService = Component.FuelTruck + Component.GroundService;
-        const string queueToVisualizer = Component.FuelTruck + Component.Visualizer; //ready
-
-        Map mapAirport = new Map();
-        
-        public double timeCoef { get; set; } = 1.0;
-        public int sleepingTime;
-        public RabbitMqClient mqClient;
-        Dictionary<int, FuelTruckCars> cars;
-        public int motionInterval = 100; //ms
-        public string planeID;
-        public int planeLocationVertex;
-        public int fuel;
-        const int carCount = 3; //Количество машинок
-        public string logMessage = "Проинициализировались, кол-во машинок: " + carCount;
-
-        delegate void GoToVertexAction(FuelTruckCars ftc, int DestinationVertex);
-        PlayDelaySource source;
+        Dictionary<string, string> queuesFrom;
+        Dictionary<string, string> queuesTo;
+        ConcurrentDictionary<string, FollowMeCar> cars;
         ConcurrentDictionary<string, CancellationTokenSource> tokens;
 
+        double timeFactor = 1;
+        int motionInterval = 100;       //ms
+        int countCars = 4;
 
-        //Конструктор. Создаем машинки
+        RabbitMqClient MqClient;
+        Map map = new Map();
+        PlayDelaySource source;
+
         public FuelTruck()
         {
+            MqClient = new RabbitMqClient();
+            cars = new ConcurrentDictionary<string, FuelTruckCar>();
             tokens = new ConcurrentDictionary<string, CancellationTokenSource>();
-            source = new PlayDelaySource(timeCoef);
-            mqClient = new RabbitMqClient();
-            Dictionary<int, FuelTruckCars> cars = new Dictionary<int, FuelTruckCars>();
-            //переделать в словарь
-            for (int i=1; i <= carCount; i++)
-            {
-                cars.Add(i, new FuelTruckCars()
-                {
-                    intCarID = i,
-                    CarID = "FuelTruck #" + i,
-                    Position = 19,
-                    Status = Status.Free,
-                    FuelOnBoard = FuelTruckCars.MaxFuelOnBoard,
-                    MotionPermitted = false
-                });
-            }
-        }    //ВАЖНАЯ ВЕЩЬ. В ДАННЫЙ МОМЕНТ НЕТ НЕОБХОДИМОСТИ В КОЛ-ВЕ ТОПЛИВА, Т.К. ОДИН МАШИНКА ЗАПРАВЛЯЕТ ОДИН САМОЛЕТ И ПОТОМ ЕДЕТ ДОМОЙ          
-
-        //Проверка свободных машинок. Возвращает id машинки. СНАЧАЛА ВСЕ МАШИНКИ ЕДУТ ДОМОЙ
-        public int CheckFreeFuelTruckCar(int fuel)
-        {
-            int numOfCar = -1;
-            while (true)
-            {                
-                foreach (FuelTruckCars ftc in cars.Values)
-                {
-                    if ((ftc.Status == Status.Free) & (ftc.FuelOnBoard > fuel)) //Free
-                    {
-                        numOfCar = ftc.intCarID;
-                        ftc.Status = Status.Busy;
-                    }
-                }
-                if (numOfCar == -1)
-                {
-                    /* ВАЖНО ПОНЯТЬ СКОЛЬКО ОЖИДАТЬ И КАК ЧАСТО ОТПРАВЛЯТЬ ЛОГИ */
-                    logMessage = "Все машины заняты, подождем 5 секунд * коэф. времени";                    
-                    SendLogMessage(logMessage);
-                    Thread.Sleep(Convert.ToInt32(5000 * timeCoef)); ///////////ВРЕМЯ СНА
-                }
-            }
-            return numOfCar;
+            source = new PlayDelaySource(timeFactor);
         }
 
-        private void WaitForMotionPermission(int curCar, int DestinationVertex)
-        {
-            mqClient.Send<MotionPermissionRequest>(Component.FuelTruck, //permission request
-                new MotionPermissionRequest()
-                {
-                    Action = MotionAction.Occupy,
-                    Component = Component.FuelTruck,
-                    DestinationVertex = DestinationVertex,
-                    ObjectId = cars[curCar].CarID,
-                    StartVertex = cars[curCar].Position
-                }) ;
-
-            while (cars[curCar].MotionPermitted)               //check if baggacar can go
-                source.CreateToken().Sleep(5);
-        }
-
-        private void MotionPermissionResponse()
-        {
-            mqClient.SubscribeTo<MotionPermissionResponse>(queueFromGroundMotion, (mpr) =>
-            {
-                for (int i = 1; i <= carCount; i++)
-                {
-                    if (cars[i].CarID == mpr.ObjectId)
-                        cars[i].MotionPermitted = true;
-                }
-            });
-        }
-
-        //Проверяем свободен ли граф. Отмечает что блокируем его
-        public bool canFuelTruckGo(int f1rs, int s3cond, int currCar) //0-полная, 1 done
-        {
-            bool yesNo = false;
-
-            //Можно ли проехать по графу?
-            var newMotionPermissionRequest = new MotionPermissionRequest()
-            {
-                Component = "FuelTruck",
-                ObjectId = cars[currCar].CarID,
-                StartVertex = f1rs,
-                DestinationVertex = s3cond,
-                Action = MotionAction.Occupy,
-            };
-            mqClient.Send(queueToGroundMotion, newMotionPermissionRequest);
-
-            ///////////тут я получаю ответ
-            mqClient.SubscribeTo<MotionPermissionResponse>(queueFromGroundMotion, (mpr) =>
-            {
-                if (mpr.ObjectId == cars[currCar].CarID)
-                {
-                    yesNo = true;
-                }                
-            });
-            //ВОПРОС! МЕТОД ЖДЕТ ОТВЕТА ИЛИ СРАЗУ ВОЗВРАЩАЕТ ЧТО-ТО? Он же понимает какой именно машинке даёт ответ
-
-            return yesNo;
-        }
-
-
-        //необходима постоянная проверка времени
-        public void FuelTruckJob(string planeID, int fuel, int planeLocationVertex)
-        {
-            int currentlyCar;
-            currentlyCar = CheckFreeFuelTruckCar(fuel); //вычислил ID свободной машинки
-
-            var shortCut = mapAirport.FindShortcut(cars[currentlyCar].Position, planeLocationVertex);//Нашел кротчайший путь
-            shortCut.Add(1000);
-            var shortCutArray = shortCut.ToArray();
-
-            for (int i = 0; i < shortCutArray.Length - 1; i++) //идём по нашему пути
-            {
-                WaitForMotionPermission();
-            }
-        }
-
-        //Отправка сообщений визуализатору
-        public void SendVisualizerMessage(string objectID, int startVertex, int destinationVertex, int speed) //возможно сделать все на +-?
-        {
-            var newVisMessage = new VisualizationMessage()
-            {
-                Type = Component.FuelTruck,
-                ObjectId = objectID,
-                StartVertex = startVertex,
-                DestinationVertex = destinationVertex,
-                Speed = speed         ///////////////speeeeed??????
-            };
-            mqClient.Send(queueToVisualizer, newVisMessage);
-
-        }
-
-        //Отправка ЛОГ сообщений
-        public void SendLogMessage(string message)
-        {
-            var newLogMessage = new LogMessage()
-            {
-                Message = message,
-                Component = "FuelTruck"
-            };
-            Console.WriteLine(message);
-            mqClient.Send(queueToLogs, newLogMessage);            
-        }
-
-        //Наше начало и подписки на время и СНО
         public void Start()
         {
-            
-            //объявление запросов
-            mqClient.DeclareQueues(queueFromTimeService, queueFromGroundService, queueFromGroundMotion,
-                queueToAirPlane, queueToLogs, queueToGroundMotion, queueToGroundService, queueToVisualizer);
-
-            SendLogMessage(logMessage);
-
-            
-            //время
-            mqClient.SubscribeTo<NewTimeSpeedFactor>(queueFromTimeService, (mes) =>
-            {
-                timeCoef = mes.Factor;
-                logMessage = "Коэффициент времени изменился и стал: " + timeCoef;
-                SendLogMessage(logMessage);
-            });
-
-            mqClient.SubscribeTo<RefuelServiceCommand>(queueFromGroundService, (mes) =>
-            {
-                FuelTruckJob(mes.PlaneId, mes.Fuel, mes.PlaneLocationVertex);
-                logMessage = String.Format("Запустили машинку в путь, PlaneID: " +
-                    "{0}, Fuel: {1}, PlaneLocation: {2}", mes.PlaneId, mes.Fuel, mes.PlaneLocationVertex);
-                SendLogMessage(logMessage);
-            });
+            CreateQueues();
+            DeclareQueues();
+            MqClient.PurgeQueues(queuesFrom.Values.ToArray());
+            Subscribe();
+            FillCollections();
         }
-
-        private void TakeTimeSpeedFactor()
+        void FillCollections()
         {
-            mqClient.SubscribeTo<NewTimeSpeedFactor>(queueFromTimeService, (ntsf) =>
+            for (int i = 0; i < countCars; i++)
             {
-                timeCoef = ntsf.Factor;
-                source.TimeFactor = timeCoef;
-            });
+                var fueltruck = new FuelTruckCar(i);
+                cars.TryAdd(fueltruck.FuelTrackId, fueltruck);
+            }
         }
+
+        void CreateQueues()
+        {
+            queuesFrom = new Dictionary<string, string>()
+            {
+                { Component.GroundMotion,Component.GroundMotion+Component.FuelTruck },
+                { Component.Airplane,Component.Airplane+Component.FuelTruck },
+                { Component.GroundService,Component.GroundService+Component.FuelTruck },
+                { Component.TimeService,Component.TimeService + Component.FuelTruck }
+            };
+            queuesTo = new Dictionary<string, string>()
+            {
+                { Component.Airplane,Component.FuelTruck+Component.Airplane },
+                { Component.Logs,Component.Logs+Component.FuelTruck },
+                { Component.GroundService,Component.FuelTruck+Component.GroundService },
+                { Component.GroundMotion,Component.FuelTruck+Component.GroundMotion },
+                { Component.Visualizer,Component.Visualizer }
+            };
+        }
+
+        void DeclareQueues()
+        {
+            MqClient.DeclareQueues(queuesFrom.Values.ToArray());
+            MqClient.DeclareQueues(queuesTo.Values.ToArray());
+        }
+
+
 
 
     }
--- AirportProject/FuelTrack/FuelTruckCars.cs
+++ AirportProject/FuelTrack/FuelTruckCars.cs
@@ -8,22 +8,27 @@ namespace FuelTruck
     {
         Free, Busy
     }
-    class FuelTruckCars
+    class FuelTruckCar
     {
-        public string CarID { get; set;  }
-        public int intCarID { get; set; }
-        
-        public Status Status { get; set; } //0 - свободна
+        string fuelTruckId { get; set;  }
+        public FuelTruckCar(int id)
+        {
+            fuelTruckID = "FuelTruck" + id;
+            MotionPermitted = false;
+            GotAirplaneResponse = false;
+        }
 
-        public static int Speed { get => 50; }
+        public string FuelTrackId { get => fuelTruckId; }
+        public string PlaneId { get; set; }
+        public Status Status { get; set; }
+        public int FuelOnBoard { get; set; }
+
+        const int MaxFuel = 1000;
+        
         public bool MotionPermitted { get; set; }
-        public int Position { get; set; }
-        public int FuelOnBoard;
-        public const int MaxFuelOnBoard = 1000;
-
-        
-
-
+        public bool GotAirplaneResponse { get; set; }
+        public static int Speed { get => 50; }
+        public int LocationVertex { get; set; }
 
     }
 }
