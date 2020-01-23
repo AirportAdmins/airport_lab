5a1f75b29b597d605a24acf1031707615581900d
Vyacheslav Dryuchin <vyacheslavdry@yandex-team.ru>
2020-01-24 00:47:42 +0300

    Добавил очередь со скоростью времени

--- visualizer/client/main.js
+++ visualizer/client/main.js
@@ -1,7 +1,6 @@
 const WIDTH = 900;
 const HEIGHT = 700;
 const SUPER_ANGLE = 180 + 90;
-
 const AIRPLAN_WIDTH=30;
 const AIRPLAN_HEIGHT=30;
 
@@ -48,18 +47,22 @@ Images[TYPES_AUTO.fueltruck].src = 'image/zheltaya.png';
 var socket = io();
 
 socket.on('visualizer', function(msg){
-    console.log(msg)
+    console.log('visualizer', msg)
     if (msg.id){
         const airplane = items.find(el=>el.id === msg.id);
         if (airplane){
             airplane.setAnimation(getPointById(msg.start), getPointById(msg.end), msg.speed);
         } else {
             const newItem = new Item(msg.id, getPointById(msg.start), msg.type);
-            
             newItem.setAnimation(getPointById(msg.start), getPointById(msg.end), msg.speed);
             items.push(newItem);
         }
     } 
+  });
+
+  socket.on('timeservicevisualizer', function(msg){
+    console.log('timeservicevisualizer', msg)
+    factor = msg.factor
   });
 
 var stage = new Konva.Stage({
@@ -67,44 +70,27 @@ var stage = new Konva.Stage({
     width: WIDTH,
     height: HEIGHT
 });
-requestAnimationFrame(function animate(time) {
-        items.forEach(item=>item.animate(time));
 
-    requestAnimationFrame(animate);
-    stage.batchDraw()
+requestAnimationFrame(function animate(time) {
+      items.forEach(item => item.animate(time));
+      requestAnimationFrame(animate);
   });
 
 var layer = new Konva.Layer();
 var laverText = new Konva.Layer();
 var autoLayer = new Konva.Layer();
+var garageLayer = new Konva.Layer();
 const points = getPoints(WIDTH, HEIGHT);
 
-setLines(layer);
 setPoints(layer);
-
-const texture = new Image();
-texture.src = 'image/texture.jpg'
-texture.onload = () => {
-  var tex = new Konva.Image({
-    x: 0,
-    y: 0,
-    image: texture,
-    width: 900,
-    height: 700
-  });
-
-  laverText.add(tex);
-  laverText.batchDraw();
-
-}
-
+setTexture();
 
 stage.add(laverText);
 
 stage.add(layer);
 
 stage.add(autoLayer);
-
+stage.add(garageLayer);
 
 
   class Item {
@@ -124,10 +110,11 @@ stage.add(autoLayer);
           });
           if (Object.values(TYPES_AUTO).includes(type)){
             autoLayer.add(this.image);
+            autoLayer.batchDraw();
           } else{
             layer.add(this.image);
+            layer.batchDraw();
           }
-          stage.batchDraw()
       }
       setAnimation(startPoint,endPoint, speed){
           this.image.rotation(getAngle(startPoint, endPoint))
@@ -137,16 +124,23 @@ stage.add(autoLayer);
           this.speed = speed;
       }
       animate(thisTime){
-          if (this.endPoint && this.speed && this.startAnimateTime){
+          if (this.endPoint && this.speed !== undefined && this.startAnimateTime){
+              if (this.speed === 0){
+                this.image.x(this.endPoint.x)
+                this.image.y(this.endPoint.y)
+                layer.batchDraw();
+                autoLayer.batchDraw();
+                this.deleteAnimation();
+                return;
+              }
               if (this.startPoint === this.endPoint){
                 this.deleteAnimation();
                 return;
               }
             const angle = Math.atan2(this.startPoint.y - this.endPoint.y, this.startPoint.x - this.endPoint.x)
-            
-
             let newX = Math.round(this.startPoint.x - this.speed*factor * (thisTime - this.startAnimateTime)* Math.cos(angle));
             let newY = Math.round(this.startPoint.y - this.speed*factor * (thisTime - this.startAnimateTime)* Math.sin(angle));
+
             if (this.endPoint.x > this.startPoint.x && newX >= this.endPoint.x || this.endPoint.x < this.startPoint.x && newX <= this.endPoint.x){
                 newX = this.endPoint.x;
             }
@@ -156,8 +150,23 @@ stage.add(autoLayer);
             if (newX == this.endPoint.x && newY == this.endPoint.y){
                 this.deleteAnimation();
             } else {
+              if (this.image.x() !== newX){
                 this.image.x(newX)
+                layer.batchDraw();
+                autoLayer.batchDraw();
+
+              } 
+              if (this.image.y() !== newY){
                 this.image.y(newY)
+                layer.batchDraw();
+                autoLayer.batchDraw();
+              }
+              // if (this.image.x() !== newX || this.image.y() !== newY){
+              //   this.image.to({
+              //     x: newX,
+              //     y: newY
+              //   })
+              // }
             }
           }
       }
@@ -173,25 +182,6 @@ stage.add(autoLayer);
   }
 
 
-  function setLines (thisLayer){
-    lines.forEach((line) => {
-        const pointsCords = line.map((id) => {
-            return points.find(el => el.id === id);
-        }).reduce((accum, el) => {
-            accum.push(el.x, el.y)
-            return accum;
-        }, []);
-    
-        var road = new Konva.Line({
-            points: pointsCords,
-            stroke: 'lightgray',
-            strokeWidth: 10,
-            lineCap: 'round',
-            lineJoin: 'round'
-        });
-        thisLayer.add(road);
-    })
-  }
 
   function setPoints(thisLayer) {
     points.forEach(point => {
@@ -207,21 +197,51 @@ stage.add(autoLayer);
         thisLayer.add(circle);
     })
   }
-  function setTexture(thisLayer){
+
+  function setTexture(){
+    const terminal = new Image();
+    terminal.src = 'image/terminal.png'
+    terminal.onload = () => {
+      const tex = new Konva.Image({
+        x: 300,
+        y: 580,
+        width: 200,
+        height: 130,
+        image: terminal,
+      });
+      garageLayer.add(tex);
+      garageLayer.batchDraw();
+    }
+
+    const garage =  new Image();
+    garage.src = 'image/garazh.png';
+
+    garage.onload = () => {
+      const tex = new Konva.Image({
+        x: 10,
+        y: 80,
+        width: 100,
+        height: 420,
+        image: garage,
+      });
+      garageLayer.add(tex);
+      garageLayer.batchDraw();
+    }
+
     const texture = new Image();
     texture.src = 'image/texture.jpg'
-
     texture.onload = ()=>{
-      var tex = new Konva.Image({
+      const tex = new Konva.Image({
         x: 0,
         y: 0,
         image: texture,
         width: 900,
         height: 700
       });
-      thisLayer.add(tex);
-      thisLayer.batchDraw();
+      laverText.add(tex);
+      laverText.batchDraw();
     }
+
   }
 
 function getPointById(id){
