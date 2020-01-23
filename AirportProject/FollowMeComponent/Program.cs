8203007eb266ac58635bbce5c1a82dc23b6170da
ChebnevDev <chebnevdev@gmail.com>
2020-01-24 02:48:22 +0300

    Fixes

--- AirportProject/FollowMeComponent/Program.cs
+++ AirportProject/FollowMeComponent/Program.cs
@@ -11,9 +11,7 @@ namespace FollowMeComponent
     {
         static void Main(string[] args)
         {
-            PlayDelaySource source = new PlayDelaySource(1);
-            var token = source.CreateToken();
-            token.Sleep(10000);
+            new FollowMeComponent().Start();
         }
     }
 }
--- stop.bat
+++ stop.bat
@@ -1,6 +1,5 @@
 rem Specify components you want to kill
 for /F %%x in (components.txt) do (
 	taskkill /F /IM %%x.exe
+	taskkill /F /IM cmd.exe /FI "WINDOWTITLE eq .*%%x.*"
 )
-
-taskkill /F /IM VsDebugConsole.exe
